f0e0b531c18b26a2c8cfcac4d981eecc23cda7f2
elleouvre <louellameish.khu@gmail.com>
2025-11-27 09:31:34 +0800

    minimal changes

--- GameManager.java
+++ GameManager.java
@@ -499,7 +499,7 @@ public class GameManager {
 
         while (!battleSystem.isBattleOver(player1, player2)) {
             System.out.println();
-            System.out.println(yellow + "==== Turn " + turnCounter + " ====" + reset);
+            System.out.println(lightYellow + "▰▰▰ Turn " + turnCounter + " ▰▰▰" + reset);
 
             Character currentPlayer = players[currentTurn % 2];
             Character opponent = players[(currentTurn + 1) % 2];
